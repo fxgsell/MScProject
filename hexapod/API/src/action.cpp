846ae73f614c3acfa29c0e80a88d75ec8f8b2086
Raspberry Pi <fxgsell@gmail.com>
2013-03-27 22:01:29 +0000

    Recalibrate leggs

--- hexapod/API/src/action.cpp
+++ hexapod/API/src/action.cpp
@@ -4,44 +4,108 @@
 #include "event.hpp"
 #include "robot.hpp"
 
-std::list<Event *> *flipOver() {
-  std::list<Event *> *flipOver = new std::list<Event *>;
+std::list<Event *> *reverseStandUp() {
+  std::list<Event *> *reverseStandUp = new std::list<Event *>;
+  reverseStandUp->push_back(new ELegSet(*robot, Leg::ALL, 0, 1000, 0));   // REVERSE GROUND
+
+  reverseStandUp->push_back(new ESleep(100000));
+
+  int a = 0, b = 0;
+  for (int i = 0; i < 20; i++) {
+    reverseStandUp->push_back(new ELegSet(*robot, Leg::A, 0,  1000, i * 50)); 
+    reverseStandUp->push_back(new ESleep(100000));
+    reverseStandUp->push_back(new ELegSet(*robot, Leg::B, 0,  1000, i * 50)); 
+    reverseStandUp->push_back(new ESleep(100000));
+
+    a = i * 50;
+    if (a > 1000)
+      a = 1000;
+  }
+
+//reverseStandUp->push_back(new ELegSet(*robot, Leg::ALL, 0, 1000, -1000));   // REVERSE GROUND
 
 /*
-  flipOver->push_back(new ELegSet(*robot, Leg::ALL, 0, 1000, -100));
-  flipOver->push_back(new ESleep(100000));
-  flipOver->push_back(new ELegSet(*robot, Leg::A, 0, 1000, -500));
-  flipOver->push_back(new ESleep(10000));
-  flipOver->push_back(new ELegSet(*robot, Leg::B, 0, 1000, -500));
-  flipOver->push_back(new ESleep(10000));
-  flipOver->push_back(new ELegSet(*robot, Leg::A, 0, 1000, -700));
-  flipOver->push_back(new ESleep(10000));
+  reverseStandUp->push_back(new ELegSet(*robot, Leg::A  , 0, 1000, 0)); 
+  reverseStandUp->push_back(new ESleep(100000));
+  reverseStandUp->push_back(new ELegSet(*robot, Leg::B  , 0, 1000, 0)); 
+  reverseStandUp->push_back(new ESleep(100000));
+  reverseStandUp->push_back(new ELegSet(*robot, Leg::A  , 0, 1000, 0)); 
+  reverseStandUp->push_back(new ESleep(100000));
+  reverseStandUp->push_back(new ELegSet(*robot, Leg::B  , 0, 1000, 0)); 
+  reverseStandUp->push_back(new ESleep(100000));
 */
 
-  flipOver->push_back(new ELegSet(*robot, Leg::ALL, 0, 1000, -700));
-  flipOver->push_back(new ESleep(10000));
+/*
+  reverseStandUp->push_back(new ELegSet(*robot, Leg::B, 0, 1000, -0)); // ELBOW AT MAX
+  reverseStandUp->push_back(new ESleep(100000));
+  reverseStandUp->push_back(new ELegSet(*robot, Leg::A, 0, 200, -1000)); // WRIST AT MAX
+  reverseStandUp->push_back(new ESleep(100000));
+*/
 
+/*
+  reverseStandUp->push_back(new ELegSet(*robot, Leg::B, 0, 1000, -700)); // ELBOW AT MAX
+  reverseStandUp->push_back(new ESleep(100000));
+  reverseStandUp->push_back(new ELegSet(*robot, Leg::A, 0, 700, -1000)); // WRIST AT MAX
+  reverseStandUp->push_back(new ESleep(100000));
+
+  reverseStandUp->push_back(new ELegSet(*robot, Leg::B, 0, 1000, -800)); // ELBOW AT MAX
+  reverseStandUp->push_back(new ESleep(100000));
+  reverseStandUp->push_back(new ELegSet(*robot, Leg::A, 0, 800, -1000)); // WRIST AT MAX
+  reverseStandUp->push_back(new ESleep(100000));
+
+  reverseStandUp->push_back(new ELegSet(*robot, Leg::B, 0, 1000, -900)); // ELBOW AT MAX
+  reverseStandUp->push_back(new ESleep(100000));
+  reverseStandUp->push_back(new ELegSet(*robot, Leg::A, 0, 900, -1000)); // WRIST AT MAX
+  reverseStandUp->push_back(new ESleep(100000));
+
+  reverseStandUp->push_back(new ELegSet(*robot, Leg::B, 0, 1000, -1000)); // ELBOW AT MAX
+  reverseStandUp->push_back(new ESleep(100000));
+  reverseStandUp->push_back(new ELegSet(*robot, Leg::A, 0, 1000, -1000)); // WRIST AT MAX
+  reverseStandUp->push_back(new ESleep(100000));
+*/
+
+ return reverseStandUp;
+
+}
+
+std::list<Event *> *flipOver() {
+  std::list<Event *> *flipOver = new std::list<Event *>;
+  std::list<Event *> *a;
+
+// STANDUP ON BACK
+//  a = reverseStandUp();
+//  flipOver->insert(flipOver->end(), a->begin(), a->end());
+//  flipOver->push_back(new ESleep(10000000));
+
+
+
+// ALL GOOD FROM HERE
+// MAULAL SET IN RIGHT POSITION
+flipOver->push_back(new ELegSet(*robot, Leg::ALL, 0, 1000, -1000));   // REVERSE GROUND
+flipOver->push_back(new ESleep(5000000));
+
+// BACK TO SIDE
 { // SIDE STAND
-
-
   // LM IN
   // LF IN
   flipOver->push_back(new ELegSet(*robot, Leg::MR, 0, -1000, -700));
   flipOver->push_back(new ELegSet(*robot, Leg::FR, 0, -1000, -700));
+  flipOver->push_back(new ESleep(100000));
 
   // MR INTERNEDIATE
   // BL INTERNEDIATE
   flipOver->push_back(new ELegSet(*robot, Leg::ML, 0, 1000, -1000));
   flipOver->push_back(new ELegSet(*robot, Leg::BR, 0, 1000, -1000));
+  flipOver->push_back(new ESleep(100000));
 
   // FR OUT
   // BR OUT
-  flipOver->push_back(new ELegSet(*robot, Leg::FL, 0, 1000, -1000));
-  flipOver->push_back(new ELegSet(*robot, Leg::BL, 0, 1000, -1000));
+  flipOver->push_back(new ELegSet(*robot, Leg::FL, 0, 900, -900));
+  flipOver->push_back(new ELegSet(*robot, Leg::BL, 0, 900, -900));
   flipOver->push_back(new ESleep(100000));
 }
 
-
+//SIDE TO NORMAL
 { //PUSH
   flipOver->push_back(new ELegSet(*robot, Leg::FL, 0, 0, 0));
   flipOver->push_back(new ELegSet(*robot, Leg::BL, 0, 0, 0));
@@ -49,11 +113,11 @@ std::list<Event *> *flipOver() {
 
 }
 
-  std::list<Event *> *a = standUp();
-  flipOver->insert(flipOver->end(), a->begin(), a->end());
-  flipOver->push_back(new ESleep(100000));
+//  STANDUP
   a = standUp();
   flipOver->insert(flipOver->end(), a->begin(), a->end());
+  flipOver->push_back(new ESleep(100000));
+
   return flipOver;
 }
 
@@ -100,18 +164,51 @@ std::list<Event *> *hello(Leg::LegId) {
 std::list<Event *> *standUp() {
   std::list<Event *> *standUp = new std::list<Event *>;
 
-  standUp->push_back(new ELegSet(*robot, Leg::A, 0, 0, 0));
-  standUp->push_back(new ELegSet(*robot, Leg::B, 0, 500, 500));
+  standUp->push_back(new ELegSet(*robot, Leg::ALL, 0, 1000, 1000));
   standUp->push_back(new ESleep(100000));
-  standUp->push_back(new ELegSet(*robot, Leg::A, 0, 500, 500));
-  standUp->push_back(new ELegSet(*robot, Leg::B, 0, 0, 0));
+  standUp->push_back(new ELegSet(*robot, Leg::ALL, 0, 0, 500));
   standUp->push_back(new ESleep(100000));
-  standUp->push_back(new ELegSet(*robot, Leg::A, 0, -300, -300));
-  standUp->push_back(new ELegSet(*robot, Leg::B, 0, 0, 0));
+
+  standUp->push_back(new ELegSet(*robot, Leg::A, 0, 200, 200));
   standUp->push_back(new ESleep(100000));
-  standUp->push_back(new ELegSet(*robot, Leg::A, 0, -300, -300));
-  standUp->push_back(new ELegSet(*robot, Leg::B, 0, -300, -300));
+  standUp->push_back(new ELegSet(*robot, Leg::A, 0, 0, 200));
+  standUp->push_back(new ESleep(500000));
+
+  standUp->push_back(new ELegSet(*robot, Leg::B, 0, 200, 200));
   standUp->push_back(new ESleep(100000));
+  standUp->push_back(new ELegSet(*robot, Leg::B, 0, -200, 200));
+  standUp->push_back(new ESleep(500000));
+
+ return standUp;
+  standUp->push_back(new ELegSet(*robot, Leg::A, 0, 600, 1000));
+  standUp->push_back(new ESleep(100000));
+  standUp->push_back(new ELegSet(*robot, Leg::B, 0, 800, 1200));
+  standUp->push_back(new ELegSet(*robot, Leg::B, 0, 600, 1000));
+  standUp->push_back(new ESleep(100000));
+  standUp->push_back(new ELegSet(*robot, Leg::A, 0, 600, 1200));
+  standUp->push_back(new ELegSet(*robot, Leg::A, 0, 400, 1000));
+  standUp->push_back(new ESleep(100000));
+  standUp->push_back(new ELegSet(*robot, Leg::B, 0, 600, 1000));
+  standUp->push_back(new ELegSet(*robot, Leg::B, 0, 400, 800));
+
+  standUp->push_back(new ELegSet(*robot, Leg::ALL, 0, -600, 200));
+
+ return standUp;
+  standUp->push_back(new ELegSet(*robot, Leg::A, 0, 800, 1000));
+  standUp->push_back(new ELegSet(*robot, Leg::B, 0, 800, 1000));
+  standUp->push_back(new ESleep(100000));
+  standUp->push_back(new ELegSet(*robot, Leg::A, 0, 800, 1000));
+  standUp->push_back(new ELegSet(*robot, Leg::B, 0, 800, 1000));
+  standUp->push_back(new ESleep(100000));
+  standUp->push_back(new ELegSet(*robot, Leg::A, 0, 800, 1000));
+  standUp->push_back(new ELegSet(*robot, Leg::B, 0, 800, 1000));
+  standUp->push_back(new ESleep(100000));
+  standUp->push_back(new ELegSet(*robot, Leg::A, 0, 800, 1000));
+  standUp->push_back(new ELegSet(*robot, Leg::B, 0, 800, 1000));
+  standUp->push_back(new ESleep(100000));
+
+  standUp->push_back(new ELegSet(*robot, Leg::ALL, 0, -600, 200));
+
   return standUp;
 }
 
--- hexapod/API/src/main.cpp
+++ hexapod/API/src/main.cpp
@@ -12,24 +12,24 @@ Body *robot;
 */
 int main() {
   Servo s0( 0,    75, false, 0);
-  Servo s1( 1,    60, false, 700); // + up
+  Servo s1( 1,  -700, false, 700); // + up
   Servo s2( 2,     0, false, 300); // ERROR
-  Servo s4( 4,   -45, false, 0);
-  Servo s5( 5,    30, false, 700); //
+  Servo s4( 4,   -50, false, 0);
+  Servo s5( 5,  -450, false, 700); //
   Servo s6( 6,     0, false, 300); // + in
   Servo s8( 8,   -25, false, 0);
-  Servo s9( 9,     0, false, 700);
-  Servo s10(10,   35, false, 300); // + in
+  Servo s9( 9,  -700, false, 700);
+  Servo s10(10, -300, false, 300); // + in
 
   Servo s18(18,   55, true,    0);
-  Servo s17(17, -150, true,  700);
-  Servo s16(16,  -50, true,  300);
+  Servo s17(17, -500, true,  700);
+  Servo s16(16, -100, true,  300);
   Servo s22(22,  -60, true,    0);
-  Servo s21(21,  -75, true,  700);
-  Servo s20(20,  120, true,  300);
+  Servo s21(21, -600, true,  700);
+  Servo s20(20,  -50, true,  300);
   Servo s26(26,  -25, true,    0);
-  Servo s25(25,   50, true,  700);
-  Servo s24(24,  -75, true,  300);
+  Servo s25(25, -500, true,  700);
+  Servo s24(24,  -50, true,  300);
 
   Leg  fr(Leg::FR, s18, s17, s16);
   Leg  mr(Leg::MR, s22, s21, s20);
@@ -39,14 +39,18 @@ int main() {
   Leg  bl(Leg::BL, s8, s9, s10);
 
   robot = new Body(fr, mr, br, fl, ml, bl);
+  robot->commit();
 
 /*
+*/
   robot->events.push_back(new ESleep(1000000));
   robot->addAction(standUp());
-  robot->addAction(standDown());
-  robot->addAction(walk(2));
-*/
+//  robot->addAction(standDown());
+//  robot->addAction(walk(2));
+
+/*
   robot->addAction(flipOver());
+*/
   robot->start();
 
 /*
