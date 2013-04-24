b5104778b72badc481fbdd679404b4dcdea20ae7
Francois-Xavier Gsell <fxgsell@gmail.com>
2013-04-24 11:46:10 +0100

    [ROBOT] More buttons

--- Protocol/protocol.h
+++ Protocol/protocol.h
@@ -9,10 +9,14 @@ struct packet {
   int height;      // Axis 3
 };
 
-#define B10 1
-#define B11 1 << 1
-#define B07 1 << 2
-#define B06 1 << 3
+#define B01 1
+#define B02 1 << 1
+#define B03 1 << 2
+#define B04 1 << 3
+#define B05 1 << 4  // CLEAR EVENTS
+#define B06 1 << 5  // RESET POSITION
+#define B11 1 << 6  // STANDUP
+#define B12 1 << 7  // SITDOWN
 
 #define BUFLEN 512
 #define NPACK 10
