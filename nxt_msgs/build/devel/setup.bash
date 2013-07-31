acb6489658e10e5bf12811e4d72f3feba48b96e2
tim <tim@tim-Inspiron-N7010.(none)>
2013-07-31 15:12:23 +0100

    Clean up.

--- nxt_msgs/build/devel/setup.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin/cmake/templates/setup.bash.in
-
-CATKIN_SHELL=bash
-. "/opt/ros/groovy/share/nxt/nxt_msgs/build/devel/setup.sh"
--- nxt_msgs/build/devel/setup.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-# generated from catkin/cmake/templates/setup.zsh.in
-
-CATKIN_SHELL=zsh
-emulate sh # emulate POSIX
-. "/opt/ros/groovy/share/nxt/nxt_msgs/build/devel/setup.sh"
-emulate zsh # back to zsh mode
