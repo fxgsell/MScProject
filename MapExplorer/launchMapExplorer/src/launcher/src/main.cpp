c70869fbb2a8477616d7b0efc879947fa536de23
root <root@tim-Inspiron-N7010.(none)>
2013-08-18 19:28:47 +0100

    Cleaned up folders.

---
+++ MapExplorer/launchMapExplorer/src/launcher/src/main.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+
+#include <errno.h>
+#include <string.h>
+#include "PackageManager.h"
+#include "ProcessManager.h"
+#include "Suscriber.hpp"
+#include <unistd.h>
+#include <fcntl.h>
+#include "JsonParser.h"
+
+int              main(int ac, char **av) {
+  PackageManager pkgMan;
+  JsonParser     parser;
+  std::string    cmd, topic, type;
+  int            cpt;
+  int            size = parser.getSize();;
+
+  while (parser.parseNextPackage(cmd, topic, type, cpt)) {
+    std::cout << "launch package:" << cmd << "   " << topic << "   " << type <<std::endl;
+    pkgMan.launchPackage(cmd, topic, type);
+    std::cout << "Package " << cpt << "/" << size << " launched..." << std::endl;
+    sleep(1);
+  }
+  std::cout << std::endl << "<<< MscProject launched >>>" << std::endl << std::endl;
+  return 0;
+}
